dc27ae40cc89889185acb2df6f4c3243576c70f6
MikeFernando <mikera2020@outlook.com>
2024-05-29 20:00:02 -0300

    Caso de Uso e Design Patterns - Criação de um usuário

--- prisma/migrations/20240529225207_create_users/migration.sql
+++ prisma/migrations/20240529225207_create_users/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
