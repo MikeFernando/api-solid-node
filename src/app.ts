6a4f1faf1fbf16d5400197ba25a1eafd5e18eea6
MikeFernando <mikera2020@outlook.com>
2024-05-29 18:14:15 -0300

    configuração prisma e docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Mike Fernando',
+    email: 'mikefernando@gmail.com',
+  },
+})
